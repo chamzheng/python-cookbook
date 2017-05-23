42951218ca0ffe412536ca8d670bc06215972ed0
chamzheng <chamzheng@foxmail.com>
2017-05-23 18:03:27 +0800

    c02

--- c02_strings_and_text/p20_perform_text_operations_on_byte_string.py
+++ c02_strings_and_text/p20_perform_text_operations_on_byte_string.py
@@ -9,4 +9,16 @@
 data = b'Hello World'
 print(data[0:5])  # b'Hello'
 
+print(data.startswith(b'Hello'))  # True
 
+print(data.split())  # [b'Hello', b'World']
+
+print(data.replace(b'Hello', b'Hello Cruel'))  # b'Hello Cruel World'
+
+# 这些操作同样也适用于字节数组。比如：
+
+data = bytearray(b'Hello World')
+print(data[0:5])  # bytearray(b'Hello')
+print(data.startswith(b'Hello'))  # True
+print(data.split())  # [bytearray(b'Hello'), bytearray(b'World')]
+print(data.replace(b'Hello', b'Hello Cruel'))  # bytearray(b'Hello Cruel World')
